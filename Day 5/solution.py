d35e0b2863de82144ea0a00012b134e8aa223a06
Kolby <mato.kollar2004@gmail.com>
2022-12-05 16:28:20 +0100

    Not Working

--- Day 5/solution.py
+++ Day 5/solution.py
@@ -6,37 +6,47 @@ commands = []
 
 def ProcessInput():
     for i, line in enumerate(lines):
-        value = ""
-        stackIndex = 1
-        if i == 0:
+        if i < 8:
+            stackIndex = 1
             for index in range(1, len(line), 4):
-                stacks[stackIndex] = []
-                if line[index] != " ":
-                    stacks[stackIndex].append(line[index])
-                stackIndex += 1
-        elif i < 8:
-            for index in range(1, len(line), 4):
-                if line[index] != " ":
-                    stacks[stackIndex].append(line[index])
+                if i == 0:
+                    stacks[stackIndex] = []
+                    if line[index] != " ":
+                        stacks[stackIndex].append(line[index])
+                else:
+                    if line[index] != " ":
+                        stacks[stackIndex].append(line[index])
+                print(stackIndex)
                 stackIndex += 1
         elif line[0] == "\n" or line[0] == " ":
             continue
         else:
-            splitLine = line.split(" ")
-            command = {splitLine[0]: splitLine[1], splitLine[2]: splitLine[3], splitLine[4]: splitLine[5][:-1]}
-            commands.append(command)
+            if i == len(lines) - 1:
+                splitLine = line.split(" ")
+                command = {splitLine[0]: int(splitLine[1]), splitLine[2]: int(splitLine[3]), splitLine[4]: int(splitLine[5])}
+                print(command)
+            else:
+                splitLine = line.split(" ")
+                lastInt = splitLine[5][:-1]
+                command = {splitLine[0]: int(splitLine[1]), splitLine[2]: int(splitLine[3]), splitLine[4]: int(lastInt)}
+                commands.append(command)
 
 
 def Part1():
+    answer = ""
     for command in commands:
         for crateIndex in range(command["move"]):
             stacks[command["to"]].insert(0, stacks[command["from"]][0])
             stacks[command["from"]].pop(0)
+    for stack in stacks.values():
+        if len(stack) > 0:
+            answer += stack[0]
+        else:
+            answer += " "
+    return answer
 
 
 ProcessInput()
-print(commands)
-# print(Part1())
-# 538
-# print(Part2())
-# 792
+print(stacks)
+print(Part1())
+# PTWLTDJ V
