4ac3b8e9ca16bc991dafb6a95aa5101cc16a2058
Kolby <mato.kollar2004@gmail.com>
2022-12-05 16:32:13 +0100

    Removed debug prints

--- Day 5/solution.py
+++ Day 5/solution.py
@@ -6,7 +6,9 @@ commands = []
 
 def ProcessInput():
     for i, line in enumerate(lines):
-        if i < 8:
+        if line[0] == "\n" or line[0] == " ":
+            continue
+        elif i < 8:
             stackIndex = 1
             for index in range(1, len(line), 4):
                 if i == 0:
@@ -16,15 +18,11 @@ def ProcessInput():
                 else:
                     if line[index] != " ":
                         stacks[stackIndex].append(line[index])
-                print(stackIndex)
                 stackIndex += 1
-        elif line[0] == "\n" or line[0] == " ":
-            continue
         else:
             if i == len(lines) - 1:
                 splitLine = line.split(" ")
                 command = {splitLine[0]: int(splitLine[1]), splitLine[2]: int(splitLine[3]), splitLine[4]: int(splitLine[5])}
-                print(command)
             else:
                 splitLine = line.split(" ")
                 lastInt = splitLine[5][:-1]
@@ -47,6 +45,5 @@ def Part1():
 
 
 ProcessInput()
-print(stacks)
 print(Part1())
 # PTWLTDJ V
